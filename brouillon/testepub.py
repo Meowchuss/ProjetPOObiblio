c13814b8c1ff78b4a5a8cb97a78ea80518e7b120
Meowchuss <meowchuss@fedora.(none)>
2022-11-13 22:48:46 +0100

    tests des epub: on a tout ce qu'il faut avec open_epub(path).toc.données

---
+++ brouillon/testepub.py
@@ -0,0 +1,14 @@
+#!/bin/env python3
+
+from epub import open_epub as op
+from pathlib import Path as p
+
+cheminLivre = p("/home/meowchuss/gitperso/ProjetPOObiblio/brouillon/livres")
+epub = []
+for l in cheminLivre.iterdir():
+    if l.suffix == ".epub":
+        epub.append(l)
+
+epub = op(epub[0])
+
+print(epub.toc.title)
