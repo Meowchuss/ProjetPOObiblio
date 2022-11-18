cdc55159ae1f09ef3e7114794ba7608d535634b5
Said Bacar El Fayed <elfayedsaidbacar@gmail.com>
2022-11-18 18:05:17 +0100

    update du projet

--- brouillon/module.py
+++ brouillon/module.py
@@ -1,10 +1,32 @@
 #!/bin/env python3
 
-# Module à importer : epub, PyPDF2, glob, os, sys
+# Module à importer : epub, PyPDF2, sys
+from pathlib import Path as p
+from PyPDF2 import PdfReader as Pdf
+from epub import open_epub as Epub
 
 class Livre():
     # possède titre, auteur, langue, nom de fichier, contenu, format
-    pass
+    def __init__(self,chemin):
+        self.path = p(chemin)
+        self.format = self.path.suffix
+        self.nom = self.path.name
+        
+        def gettoc(path): # On veut un format {"Titre" : "Nom du livre", "Chapitres": {"Chapitre i" : ["Nom du chapitre", page]}}
+            pass
+        
+        if self.format == ".pdf":
+            pdf = Pdf.reader(self.path)
+            self.auteur = pdf.metadata["Author"]
+            self.titre = pdf.metadata["Title"]
+            self.langue = 0
+            self.toc = {}
+        elif self.format == ".epub":
+            epub = Epub(self.path)
+            self.auteur = 0
+            self.titre = 0
+            self.langue = 0
+            self.toc = {}
 class Bibliothèque():
     # possède une liste d'objet Livre
     pass
--- brouillon/testPdf.py
+++ brouillon/testPdf.py
@@ -4,10 +4,11 @@ from pathlib import Path as p
 from PyPDF2 import PdfReader as Pr
 
 cheminLivre = p("/home/meowchuss/gitperso/ProjetPOObiblio/brouillon/livres")
+c = p("/.math_etu/users/2023/ds1/122000976/DepotsGithub/ProjetPOObiblio/brouillon/livres")
 pdf = []
-for l in cheminLivre.iterdir():
+for l in c.iterdir():
     if l.suffix == ".pdf":
         pdf.append(l)
-reader = Pr(pdf[1])
-page = reader.pages[7]
-print(reader.outlines)
+reader = Pr(pdf[4])
+meta = reader.outline
+print(meta)
